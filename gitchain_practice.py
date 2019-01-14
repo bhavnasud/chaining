7423f51a8980d69aa90770472296915087627d10
Bhavna Sud <bhavnasud@bhavnas-mbp.lan>
2019-01-13 18:38:30 -0800

    message

--- gitchain_practice.py
+++ gitchain_practice.py
@@ -13,16 +13,14 @@ def git_commit():
 	os.environ["GIT_AUTHOR_DATE"] = time
 	os.environ["GIT_COMMITTER_DATE"] = time
 	repo.git.add('--all')
-	repo.index.commit('noonce' + COMMIT_MESSAGE)
-	print(repo.head.object.hexsha)
-	comm = Commit(repo, repo.head.object.binsha, message = 'hello')
-	repo.commit(comm)
+	comm = Commit(repo, Commit.NULL_BIN_SHA, None, 
+      None, time, 0, 
+      None, time, 0,
+      COMMIT_MESSAGE, None, None)
 	#for x in range(0, 100000):
 	    #try:
+	comm.message = 'noonce' + COMMIT_MESSAGE
 	print(comm.hexsha)
-	#comm = Commit(repo, repo.head.object.binsha, message = 'hello2')
-	#comm.message = 'noonce' + COMMIT_MESSAGE
-	#print(comm.hexsha)
 	        #repo.index.commit('noonce' + str(x) + COMMIT_MESSAGE)
 	        #if (x % 1000 == 0):
 	        	#print(x)
